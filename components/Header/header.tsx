c8cb7258c9d1ab8a9497ff02f2c5318b62764037
Your Name~ <vatsal sorathiya>
2023-07-26 03:44:06 -0700

    working on slider modification done

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -31,11 +31,19 @@ export default function Header() {
                     </button>
                 </div>
                 <button className="btn" onClick={() => {
-                    if (window && window?.login_modal) {
-                        window?.login_modal.showModal()
-                    }
+                    window?.login_modal.showModal()
                 }}>open modal</button>
                 <Login />
+            </div>
+            <div className="drawer lg:drawer-open">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-side">
+                    <label className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 h-screen bg-base-200 text-base-content">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
             </div>
             <div className="btm-nav lg:hidden">
                 <button>
